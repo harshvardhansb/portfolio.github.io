c0e2050b1571d48ba5708f245e3f3ab649a0269e
Harsh Vardhan Singh <95876759+harshvardhansb@users.noreply.github.com>
2022-06-28 23:13:05 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('.hamburger');
+const mobile_nav = document.querySelector('.mobile-nav');
+
+
+hamburger.addEventListener('click',function(){
+
+    // this.classList.toggle('is-active')
+    hamburger.classList.toggle('is-active')
+    mobile_nav.classList.toggle('is-active')
+
+});
+
+
+
+
+
+
+// window.onload=function (){
+//     window.addEventListener('scroll',function (e) {
+//         if (window.pageYOffset>100) {
+//             document.querySelector("header").classList.add('is-scrolling');
+//         }else{
+//             document.querySelector("header").classList.remove('is-scrolling');
+//         }
+//     });
+
+//     const menu_bt = document.querySelector('.hamburger');
+
+//     menu_btn.addEventListener('click', function(){
+//     menu_btn.classList.toggle('is-active');
+    
+// });
+// }
+
